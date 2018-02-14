73c0099b5c2085647916f11536925c605f9b2b31
Tobias Johansen <tobjoh1994@gmail.com>
2018-02-14 19:19:18 +0100

    Added Riemann Zeta function to main.

--- P1/main.cpp
+++ P1/main.cpp
@@ -1,6 +1,24 @@
+#include <cmath>
 #include <iostream>
+
+int iterations = 100000000;
+void riemannZetaFunction(){
+    double s = 0;
+    for(int i = 1; i < iterations; i++){
+        s += 1/pow(i,2);
+    }
+    std::cout << "Sum of Riemann Zeta function: " << s << std::endl;
+}
+void machinFormula(){
+    double s = 0;
+    for(int i = 1; i < iterations; i++){
+        s += 1/pow(i,2);
+    }
+    std::cout << "Sum of Machin formula: " << s << std::endl;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    riemannZetaFunction();
     return 0;
 }
