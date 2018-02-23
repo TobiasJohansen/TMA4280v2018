927187ceb221a022bce15a9b9aa8102a53cbbad4
Tobias Johansen <tobjoh1994@gmail.com>
2018-02-23 15:29:32 +0100

    Cleanup

--- P1/mach0/test.cpp
+++ P1/mach0/test.cpp
@@ -2,20 +2,31 @@
 #include <cstdlib>
 #include <iostream>
 
-double machinFormula(int iterations){
-  iterations++;
+double arctan(int iterations, double x){
   double s = 0;
   for(int i = 1; i < iterations; i++){
-      s += pow(-1,i-1)*(pow(1.0,2*i-1)/(2*i-1));
+      s += pow(-1,i-1)*(pow(x,2*i-1)/(2*i-1));
   }
   return s;
 }
 
+double machinFormula(int iterations){
+  iterations++;
+  return 4*(4*arctan(iterations, 1.0/5)-arctan(iterations, 1.0/239));
+}
+
 int main(int argc, char* argv[]) {
-  int iterations;
-  if(argc != 2) return 1;
-  iterations = std::atoi(argv[1]);
-  if(iterations == 0) return 1;
-  std::cout << "Sum of Machin formula: " << machinFormula(iterations) << std::endl;
-  return 0;
+  if(argc < 2 ) return 1;
+  int task = std::atoi(argv[1]);
+  if(task == 0) return 1;
+  else if(task == 1){
+    if(argc != 3) return 1;
+    int iterations = std::atoi(argv[2]);
+    if(iterations == 0) return 1;
+    std::cout << "Approximation of pi using the Machin formula: " << machinFormula(iterations) << std::endl;
+  }else if(task == 2){
+    if(argc != 2) return 1;
+    std::cout << "Computed Machin: TODO" << std::endl;
+    std::cout << "Expected Machin: TODO" << std::endl;
+  }
 }
--- P1/zeta0/test.cpp
+++ P1/zeta0/test.cpp
@@ -8,14 +8,22 @@ double riemannZetaFunction(int iterations){
   for(int i = 1; i < iterations; i++){
     s += 1.0/pow(i,2);
   }
-  return s;
+  return sqrt(6*s);
 }
 
 int main(int argc, char* argv[]) {
-  int iterations;
-  if(argc != 2) return 1;
-  iterations = std::atoi(argv[1]);
-  if(iterations == 0) return 1;
-  std::cout << "Sum of Riemann Zeta function: " << riemannZetaFunction(iterations) << std::endl;
+  if(argc < 2 ) return 1;
+  int task = std::atoi(argv[1]);
+  if(task == 0) return 1;
+  else if(task == 1){
+    if(argc != 3) return 1;
+    int iterations = std::atoi(argv[2]);
+    if(iterations == 0) return 1;
+    std::cout << "Approximation of pi using the Riemann Zeta function: " << riemannZetaFunction(iterations) << std::endl;
+  }else if(task == 2){
+    if(argc != 2) return 1;
+    std::cout << "Computed Riemann Zeta: " << riemannZetaFunction(3) << std::endl;
+    std::cout << "Expected Riemann Zeta: " << sqrt(6*(1.0/pow(1,2)+1.0/pow(2,2)+1.0/pow(3,2))) << std::endl;
+  }
   return 0;
 }
